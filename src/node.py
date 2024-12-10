8442308dd67776324e76735ef4973ff6ba4c178b
tudorsonycx <tudorsonycx@gmail.com>
2024-12-10 23:57:10 +0000

    add add_key, remove_key, and get_random_key methods for Node class

--- src/node.py
+++ src/node.py
@@ -19,3 +19,16 @@ class Node:
         self.next = None
         self.prev = None
         self.keys = set()
+
+    def add_key(self, key):
+        self.keys.add(key)
+
+    def remove_key(self, key):
+        self.keys.remove(key)
+
+    def get_random_key(self):
+        if self.keys:
+            rand_key = self.keys.pop()
+            self.keys.add(rand_key)
+            return rand_key
+        return ""

0405d813db17c2802646d26e7b0fd09d5c27567f
tudorsonycx <tudorsonycx@gmail.com>
2024-12-10 23:59:28 +0000

    document get_random_key method in Node class

--- src/node.py
+++ src/node.py
@@ -27,6 +27,16 @@ class Node:
         self.keys.remove(key)
 
     def get_random_key(self):
+        """
+        Returns a random key from the set of keys.
+
+        If the set of keys is not empty, a random key is selected,
+        temporarily removed from the set, and then re-added to ensure
+        the set remains unchanged. The selected key is then returned.
+
+        Returns:
+            str: A random key from the set if it exists, otherwise an empty string.
+        """
         if self.keys:
             rand_key = self.keys.pop()
             self.keys.add(rand_key)
